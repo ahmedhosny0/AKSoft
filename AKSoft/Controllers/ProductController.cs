448ab4621de852c78c64306b871a1e0f3c6fd5c3
ahmedhosny0 <63015510+ahmedhosny0@users.noreply.github.com>
2022-02-05 00:38:31 +0200

    make 3 pages for unit edit show and controller

--- AKSoft/Controllers/ProductController.cs
+++ AKSoft/Controllers/ProductController.cs
@@ -394,6 +394,82 @@ public class ProductController : Controller
         }
         return RedirectToAction("DisplayCategories");
     }
+    // Units Action Delete Edit
+    [HttpGet]
+    public ActionResult DisplayUnits()
+    {
+        DataTable dtblProduct = new DataTable();
+        using (SqlConnection sqlCon = new SqlConnection(connectionString))
+        {
+            sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT Serial,ArabicName,EnglishName,DescName,Description FROM UnitCode", sqlCon);
+            sqlDa.Fill(dtblProduct);
+        }
+        return View(dtblProduct);
+    }
+
+    [HttpGet]
+    public ActionResult CreateUnit()
+    {
+        return View(new UnitCode());
+    }
+    public ActionResult DeleteUnit(int? id)
+    {
+        using (SqlConnection sqlCon = new SqlConnection(connectionString))
+        {
+            sqlCon.Open();
+            string query = "DELETE FROM UnitCode WHere Serial = @Serial";
+            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+            sqlCmd.Parameters.AddWithValue("@Serial", id);
+            sqlCmd.ExecuteNonQuery();
+        }
+        return RedirectToAction("DisplayUnits");
+    }
+    // Edit Categories
+    public ActionResult EditUnit(int? id)
+    {
+        UnitCode productModel = new UnitCode();
+        DataTable dtblProduct = new DataTable();
+        using (SqlConnection sqlCon = new SqlConnection(connectionString))
+        {
+            sqlCon.Open();
+            string query = "SELECT Serial,ArabicName,EnglishName,DescName,Description FROM UnitCode Where Serial = @Serial";
+            SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+            sqlDa.SelectCommand.Parameters.AddWithValue("@Serial", id);
+            sqlDa.Fill(dtblProduct);
+        }
+        if (dtblProduct.Rows.Count == 1)
+        {
+            productModel.Serial = Convert.ToInt32(dtblProduct.Rows[0][0].ToString());
+            productModel.ArabicName = dtblProduct.Rows[0][1].ToString();
+            productModel.EnglishName = dtblProduct.Rows[0][2].ToString();
+            productModel.DescName = dtblProduct.Rows[0][3].ToString();
+            productModel.Description = dtblProduct.Rows[0][4].ToString();
+            return View(productModel);
+        }
+        else
+            return RedirectToAction("DisplayUnits");
+    }
+
+    //
+    // POST: /Product/Edit/5
+    [HttpPost]
+    public ActionResult EditUnit(UnitCode productModel)
+    {
+        using (SqlConnection sqlCon = new SqlConnection(connectionString))
+        {
+            sqlCon.Open();
+            string query = "UPDATE UnitCode SET ArabicName = @ArabicName ,EnglishName = @EnglishName ,DescName=@DescName ,Description=@Description   WHere Serial = @pr";
+            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+            sqlCmd.Parameters.AddWithValue("@pr", productModel.Serial);
+            sqlCmd.Parameters.AddWithValue("@ArabicName", productModel.ArabicName);
+            sqlCmd.Parameters.AddWithValue("@EnglishName", productModel.EnglishName);
+            sqlCmd.Parameters.AddWithValue("@DescName", productModel.DescName);
+            sqlCmd.Parameters.AddWithValue("@Description", productModel.Description);
+            sqlCmd.ExecuteNonQuery();
+        }
+        return RedirectToAction("DisplayUnits");
+    }
 }
 
 
